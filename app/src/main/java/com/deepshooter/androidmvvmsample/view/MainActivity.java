fe3d6deaef593c50109d8247968f54b246cd9f5d
Avinash Mishra <Avinash.Mishra@mindtree.com>
2019-04-22 14:09:26 +0530

    Initial commit

---
+++ app/src/main/java/com/deepshooter/androidmvvmsample/view/MainActivity.java
@@ -0,0 +1,31 @@
+package com.deepshooter.androidmvvmsample.view;
+
+import android.databinding.BindingAdapter;
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.deepshooter.androidmvvmsample.R;
+import com.deepshooter.androidmvvmsample.databinding.ActivityMainBinding;
+import com.deepshooter.androidmvvmsample.viewmodel.LoginViewModel;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel(new LoginViewModel());
+        activityMainBinding.executePendingBindings();
+
+    }
+
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view, String message) {
+        if (message != null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
